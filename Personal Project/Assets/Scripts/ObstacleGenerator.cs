d2554b411a19d6d2e7498de35fd08cdde8f23f98
Joe4859 <70210975+Joe4859@users.noreply.github.com>
2020-09-16 13:21:32 -0400

    Block in Personal Level
    A prototype of what the personal level will look like. Has obtacles (that will be generated as the game goes on), two walls, a dirt road to run on, and a finish line/plane.

---
+++ Personal Project/Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Create a new cube primitive to set the color on
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        //Get the Renderer component from the new cube
+        var cubeRenderer = cube.GetComponent<Renderer>();
+
+        //Call SetColor using the shader property name "_Color" and setting the color to red
+        cubeRenderer.material.SetColor("_Color", Color.black);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
